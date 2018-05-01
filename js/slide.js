4a085f529eb59cba620ffcc23fd964a32bb4382f
Patrick Cull <paddycull9@hotmail.com>
2018-05-01 10:45:08 +0100

    Added Fade in Animation

---
+++ js/slide.js
@@ -0,0 +1,12 @@
+$(document).scroll(function () {
+    var y = $(this).scrollTop();
+    // Show element after user scrolls past 
+    // the top edge of its parent 
+    $('.fade').each(function () {
+        var t = $(this).parent().offset().top;
+        var t = t -200;
+        if (y > t) {
+            $(this).fadeIn();
+        }
+    });
+});
