393cccbe39010d13b4ded40d9182928eddb6b0a0
Patrick Cull <paddycull9@hotmail.com>
2018-05-11 15:48:17 +0100

    Added to Work Section

--- js/main.js
+++ js/main.js
@@ -54,4 +54,21 @@ jQuery(document).ready(function($){
         	300
         );
 	}
+
+
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+	
+	for (i = 0; i < acc.length; i++) {
+	  acc[i].addEventListener("click", function() {
+	    this.classList.toggle("active-acc");
+	    var panel = this.nextElementSibling;
+	    if (panel.style.maxHeight){
+	      panel.style.maxHeight = null;
+	    } else {
+	      panel.style.maxHeight = panel.scrollHeight + "px";
+	    } 
+	  });
+	}
+
 });
